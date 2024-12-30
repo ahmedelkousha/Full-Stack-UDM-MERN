ff237c6c77850ed993e9d4573fa398c30781ffe3
ahmedelkousha <elkoushaservice@gmail.com>
2024-12-30 16:08:33 +0200

    retry 6

--- server/models/Users.js
+++ server/models/Users.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   Name: {
     type: String,
- 
+    required: true,
   },
   Email: {
     type: String,
-   
+    required: true,
   },
   Age: {
     type: Number,
-    
+    required: true,
   },
 });
 
