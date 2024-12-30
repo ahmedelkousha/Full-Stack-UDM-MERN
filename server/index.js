0b4a5325a807c0aa6411586f5f52d12bfc7395c2
ahmedelkousha <elkoushaservice@gmail.com>
2024-12-30 16:00:10 +0200

    remove export mongoose

--- server/index.js
+++ server/index.js
@@ -90,7 +90,7 @@ app.post(
   })
 );
 
-module.exports = mongoose.connect(process.env.DATABASE_URL).then(() => {
+mongoose.connect(process.env.DATABASE_URL).then(() => {
   app.listen(process.env.PORT, () => {
     console.log(`Success! Server is running on port ${process.env.PORT}`);
   });
