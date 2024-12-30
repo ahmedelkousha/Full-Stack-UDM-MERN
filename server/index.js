e6eda06929825c25a28b10aa5ab2afdabfd717cf
ahmedelkousha <elkoushaservice@gmail.com>
2024-12-30 15:38:39 +0200

    export functions

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+module.exports = app.get("/", (req, res) => {
   userInfo
     .find({})
     .then((users) => {
@@ -21,59 +21,71 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/getUser/:id", (req, res) => {
-  const id = req.params.id;
-  userInfo
-    .findById({ _id: id })
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+app.get(
+  "/getUser/:id",
+  (module.exports = (req, res) => {
+    const id = req.params.id;
+    userInfo
+      .findById({ _id: id })
+      .then((users) => {
+        res.json(users);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+);
 
-app.put("/updateUser/:id", (req, res) => {
-  const id = req.params.id;
-  userInfo
-    .findByIdAndUpdate(
-      { _id: id },
-      { Name: req.body.Name, Email: req.body.Email, Age: req.body.Age }
-    )
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+app.put(
+  "/updateUser/:id",
+  (module.exports = (req, res) => {
+    const id = req.params.id;
+    userInfo
+      .findByIdAndUpdate(
+        { _id: id },
+        { Name: req.body.Name, Email: req.body.Email, Age: req.body.Age }
+      )
+      .then((users) => {
+        res.json(users);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  })
+);
 
-app.delete("/deleteUser/:id", (req, res) => {
-  const id = req.params.id;
-  userInfo
-    .findByIdAndDelete({ _id: id })
-    .then((res) => {
-      res.json(res);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+app.delete(
+  "/deleteUser/:id",
+  (module.exports = (req, res) => {
+    const id = req.params.id;
+    userInfo
+      .findByIdAndDelete({ _id: id })
+      .then((res) => {
+        res.json(res);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  })
+);
 
-app.post("/createUser", async (req, res) => {
-  await userInfo
-    .create(req.body)
-    .then((user) => {
-      console.log(req.body);
+app.post(
+  "/createUser",
+  (module.exports = async (req, res) => {
+    await userInfo
+      .create(req.body)
+      .then((user) => {
+        console.log(req.body);
 
-      res.json(user);
-      console.log("Data sent to DB Successfully!");
-    })
-    .catch((err) => {
-      res.json(err);
-      console.log("Error, Data failed to be sent to DB!", err);
-    });
-});
+        res.json(user);
+        console.log("Data sent to DB Successfully!");
+      })
+      .catch((err) => {
+        res.json(err);
+        console.log("Error, Data failed to be sent to DB!", err);
+      });
+  })
+);
 
 mongoose.connect(process.env.DATABASE_URL).then(() => {
   app.listen(process.env.PORT, () => {
