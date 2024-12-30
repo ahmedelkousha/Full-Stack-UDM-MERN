ff94f17d3d109a60455463e3f68ddfe860febd5b
ahmedelkousha <elkoushaservice@gmail.com>
2024-12-30 16:27:59 +0200

    almost done

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -7,7 +7,7 @@ export default function Users() {
     }])
 
     const handleDelete = (id) => {
-        axios.delete('http://localhost:3000/deleteUser/' + id).then((res) => {
+        axios.delete('https://udm-server.vercel.app/deleteUser/' + id).then((res) => {
             console.log(res);
 
         }).catch((err) => {
@@ -16,46 +16,46 @@ export default function Users() {
         })
     }
 
-    useEffect( () => {
-        axios.get("http://localhost:3000").then((result) => {
+    useEffect(() => {
+        axios.get("https://udm-server.vercel.app").then((result) => {
             setUsers(result.data)
-    }).catch((err) => {
-        console.log(err)
+        }).catch((err) => {
+            console.log(err)
+        })
     })
-})
 
-return (
-    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
-        <div className="bg-white rounded p-3">
-            <Link to="/create" className="btn btn-success">Add +</Link>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Age</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user, index) => (
-                        <tr key={index}>
-                            <td>{user._id}</td>
-                            <td>{user.Name}</td>
-                            <td>{user.Email}</td>
-                            <td>{user.Age}</td>
-                            <td>
-                                <Link className="btn btn-success" to={`/update/${user._id}`}>Edit</Link>
-                                <button onClick={() => {
-                                    handleDelete(user._id)
-                                }} className="btn btn-danger">Delete</button>
-                            </td>
+    return (
+        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className="bg-white rounded p-3">
+                <Link to="/create" className="btn btn-success">Add +</Link>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Action</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {users.map((user, index) => (
+                            <tr key={index}>
+                                <td>{user._id}</td>
+                                <td>{user.Name}</td>
+                                <td>{user.Email}</td>
+                                <td>{user.Age}</td>
+                                <td>
+                                    <Link className="btn btn-success" to={`/update/${user._id}`}>Edit</Link>
+                                    <button onClick={() => {
+                                        handleDelete(user._id)
+                                    }} className="btn btn-danger">Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-)
+    )
 }
